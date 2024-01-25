11e0e3b36068be9d4162aaf8ca71cc12b617a232
Dmitry Trifonov <extde@yandex.ru>
2024-01-25 08:11:48 +0000

    added plugun configuration

---
+++ custom/plugins.lua
@@ -0,0 +1,16 @@
+local plugins = {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^3', -- Recommended
+    ft = { 'rust' }
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  }
+}
+
+return plugins
